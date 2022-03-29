8fa0016fbd2a7a2dcb4ccda9a61f96d34c075a37
Marvin Mallari <201810387@gordoncollege.edu.ph>
2022-03-29 12:11:22 +0800

    Bookaholic_admin

---
+++ Component/Modals/plan_admin_modal.php
@@ -0,0 +1,49 @@
+<div class="modal" id="plan_menu_admin">
+<div class="modal-footer">
+    <button class="btn-flat modal-close" style="color:red;font-size:30px;">&times;</button>
+</div>
+<div class="modal-content">
+    <div class="row">
+         <div class="col s12 center">
+            <div id="image_prop_view"></div>
+        </div>
+        <div class="col s12" style="font-size:20px;font-family:arial;">
+            <div class="col s6">
+                <b>Full Name</b>
+                <span id="propowner"></span>
+            </div>
+            <div class="col s6">
+                <b>Book Title:</b>
+                <span id="propname"></span>
+            </div>
+            <div class="col s6">
+                <b>Email Address / Phone Number:</b>
+                <span id="propdesc"></span>
+            </div>
+            <div class="col s6">
+                <b>Price: ₱</b>
+                <span id="prop_price"></span>
+            </div>
+            <div class="col s6">
+                <b>Home Address:</b>
+                <span id="prop_address"></span>
+            </div>
+            <div class="col s6">
+                <b>Status:</b>
+                <span id="rent_stat"></span>
+            </div>
+        </div>
+
+        <div class="col s12">
+            <input type="hidden" name="" id="propertyID">
+        </div>
+        
+    </div>
+    <div class="row divider"></div>
+    <div class="row">
+    <div class="input-field col s12 center">
+        <button class="btn red" onclick="delete_plan_admin()" id="delPlanBtn">delete</button>
+    </div>
+    </div>
+</div>
+</div>
